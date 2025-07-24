61c412740226315478eb995aabd2d99e4c6e3fae
Pradana Yahya Abdillah <pradanayahyaabdillah@mail.ugm.ac.id>
2025-07-24 14:32:04 +0700

    update dependecies for streamlit deploy commun

--- src/config.py
+++ src/config.py
@@ -1,12 +1,17 @@
 import os
 from pathlib import Path
+import streamlit as st
 from dotenv import load_dotenv
 
 # load .env in root dir
 load_dotenv(Path(__file__).resolve().parent.parent / ".env")
 
 TMDB_API_KEY = os.getenv("TMDB_API_KEY", "BELOM_ADA")
-GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+if "GEMINI_API_KEY" in st.secrets:
+    GEMINI_API_KEY = st.secrets["GEMINI_API_KEY"]
+else:
+    GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+
 BASE_POSTER_URL = "https://image.tmdb.org/t/p/w500"
 
 ROOT_PATH = Path(__file__).resolve().parent.parent
